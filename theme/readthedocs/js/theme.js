217641e8ed0b815ae34b2b2ec92d0a9f89ea44cd
alexey <alexey.lazarenko@jelastic.com>
2015-12-29 10:59:39 +0200

    testcomit 0.5

--- theme/readthedocs/js/theme.js
+++ theme/readthedocs/js/theme.js
@@ -1,16 +1,20 @@
 $( document ).ready(function() {
     // Shift nav in mobile when clicking the menu.
     $(document).on('click', "[data-toggle='wy-nav-top']", function() {
-      $("[data-toggle='wy-nav-shift']").toggleClass("shift");
-      $("[data-toggle='rst-versions']").toggleClass("shift");
+        $("[data-toggle='wy-nav-shift']").toggleClass("shift");
+        $("[data-toggle='rst-versions']").toggleClass("shift");
     });
     // Close menu when you click a link.
     $(document).on('click', ".wy-menu-vertical .current ul li a", function() {
-      $("[data-toggle='wy-nav-shift']").removeClass("shift");
-      $("[data-toggle='rst-versions']").toggleClass("shift");
+        $("[data-toggle='wy-nav-shift']").removeClass("shift");
+        $("[data-toggle='rst-versions']").toggleClass("shift");
     });
     $(document).on('click', "[data-toggle='rst-current-version']", function() {
-      $("[data-toggle='rst-versions']").toggleClass("shift-up");
+        $("[data-toggle='rst-versions']").toggleClass("shift-up");
+        if ($(".fa-caret-down").length > 0)
+            $(".fa-caret-down").attr('class', 'fa fa-caret-up');
+        else
+            $(".fa-caret-up").attr('class', 'fa fa-caret-down');
     });
     // Make tables responsive
     $("table.docutils:not(.field-list)").wrap("<div class='wy-table-responsive'></div>");
